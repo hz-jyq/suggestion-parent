3c03bd4d4367a79f4a14768287fdc6cfcc4e3ddd
jinyuanqian <hzjyq@tairanchina.com>
2017-02-22 19:29:37 +0800

    add

--- core-suggestion-api/src/main/java/com/fengdai/suggestion/form/MySuggestionForm.java
+++ core-suggestion-api/src/main/java/com/fengdai/suggestion/form/MySuggestionForm.java
@@ -3,11 +3,14 @@ package com.fengdai.suggestion.form;
 import javax.ws.rs.QueryParam;
 
 import com.fengdai.base.form.AbstractPageForm;
+import com.fengdai.rest.Annotation.NotNull;
 
 public class MySuggestionForm extends AbstractPageForm {
 	
 	private static final long serialVersionUID = 5388424862764009024L;
 	
+	
+	@NotNull
 	@QueryParam("context")
 	private String context;
 
